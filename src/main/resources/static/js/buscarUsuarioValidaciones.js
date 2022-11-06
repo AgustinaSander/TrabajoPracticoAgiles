39b508ca18e02a9f77a2e2b27255dd88ca09743f
Agustina Vergara <agusv369@gmail.com>
2022-11-06 20:41:28 -0300

    Interfaces emitir licencia - buscar usuario - alta usuario

---
+++ src/main/resources/static/js/buscarUsuarioValidaciones.js
@@ -0,0 +1,35 @@
+const fields = document.querySelector("#fields_to_complete");
+const inputs = document.querySelectorAll("#fields_to_complete input");
+
+/*Search fields validation*/
+const expressions =  {
+    name: /^[a-zA-ZÀ-ÿ\s]{0,30}$/,
+    surname:/^[a-zA-ZÀ-ÿ\s]{0,30}$/,
+    dni: /^\d{8,10}$/
+}
+
+const validation = (e) => {
+    switch(e.target.name){
+        case "name":
+            validation_field(expressions.name, e.target, 'name');
+            break;
+        case "surname":
+            validation_field(expressions.surname, e.target, 'surname');
+            break;
+        case "dni":
+            validation_field(expressions.dni, e.target, 'dni');
+            break;
+    }
+}
+
+const validation_field = (expressions, input, field) =>{
+    if(expressions.test(input.value)){
+        document.querySelector(`#field_${field} .error_message`).classList.remove('message_active');
+    }else{
+        document.querySelector(`#field_${field} .error_message`).classList.add('message_active');
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('blur', validation)
+});
