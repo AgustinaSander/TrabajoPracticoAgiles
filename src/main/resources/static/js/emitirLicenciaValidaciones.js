ed4dc4bba866202c3d0b004838887febf31a7baa
Agustina Sander <asander00@hotmail.com>
2022-11-30 12:45:26 -0300

    Fix radio button emitir licencia

--- src/main/resources/static/js/emitirLicenciaValidaciones.js
+++ src/main/resources/static/js/emitirLicenciaValidaciones.js
@@ -7,39 +7,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 const fields = document.querySelector("#fields_to_complete");
 const inputs = document.querySelectorAll("#fields_to_complete input");
 
-/*Search fields validation*/
-const expressions =  {
-    name: /^[a-zA-ZÀ-ÿ\s]{0,30}$/,
-    surname:/^[a-zA-ZÀ-ÿ\s]{0,30}$/,
-    dni: /^\d{8,10}$/
-}
-
-const validation = (e) => {
-    switch(e.target.name){
-        case "name":
-            validation_field(expressions.name, e.target, 'name');
-            break;
-        case "surname":
-            validation_field(expressions.surname, e.target, 'surname');
-            break;
-        case "dni":
-            validation_field(expressions.dni, e.target, 'dni');
-            break;
-    }
-}
-
-const validation_field = (expressions, input, field) =>{
-    if(expressions.test(input.value)){
-        document.querySelector(`#field_${field} .error_message`).classList.remove('message_active');
-    }else{
-        document.querySelector(`#field_${field} .error_message`).classList.add('message_active');
-    }
-}
-
-inputs.forEach((input) => {
-    input.addEventListener('blur', validation)
-});
-
 document.getElementById("button").addEventListener('click', (e) =>{
     e.preventDefault();
     searchLicenseHolders();
@@ -69,7 +36,7 @@ function updateLicenseHoldersList(licenseholders){
     tbody.innerHTML = '';
     for(const [index, l] of licenseholders.entries()){
         tbody.innerHTML += `<tr>
-           <td><button class="btn btn-primary" onclick="selectLicenseHolder(`+l.id+`)"></td>
+           <td><input type="radio" name="radio_holders" onclick="selectLicenseHolder(`+l.id+`)"></input></td>
            <td>${l.name}</td>
            <td>${l.surname}</td>
            <td>${l.type}</td>
